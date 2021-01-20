df1b3d968bf66c747d1477c7f69778c90bde8302
staskustov <9687128685@mail.ru >
2021-01-20 13:39:49 +0300

    new changes

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,49 +1,83 @@
+import os
 from django.shortcuts import render
+from django.conf import settings
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    title = "Главная"
+    content = {'title': title}
+    return render(request, 'mainapp/index.html', content)
 
 def add_realty(request):
-    return render(request, 'mainapp/add_realty.html')
+    title = "Карточка объекта"
+    content = {'title': title}
+    return render(request, 'mainapp/add_realty.html', content)
 
 def add_client(request):
-    return render(request, 'mainapp/add_client.html')
+    title = "Карточка клиента"
+    content = {'title': title}
+    return render(request, 'mainapp/add_client.html', content)
 
 def calendar(request):
-    return render(request, 'mainapp/calendar.html')
+    title = "Календарь"
+    content = {'title': title}
+    return render(request, 'mainapp/calendar.html', content)
 
 def clients(request):
-    return render(request, 'mainapp/clients.html')
+    title = "Клиенты"
+    content = {'title': title}
+    return render(request, 'mainapp/clients.html', content)
 
 def customization(request):
-    return render(request, 'mainapp/customization.html')
+    title = "Настройки"
+    content = {'title': title}
+    return render(request, 'mainapp/customization.html', content)
 
 def deal_card(request):
-    return render(request, 'mainapp/deal_card.html')
+    title = "Карточка сделки"
+    content = {'title': title}
+    return render(request, 'mainapp/deal_card.html', content)
 
 def documents(request):
-    return render(request, 'mainapp/documents.html')
+    title = "Документы"
+    content = {'title': title}
+    return render(request, 'mainapp/documents.html', content)
 
 def information(request):
-    return render(request, 'mainapp/informationx.html')
+    title = "Информация"
+    content = {'title': title}
+    return render(request, 'mainapp/information.html', content)
 
 def mail(request):
-    return render(request, 'mainapp/mail.html')
+    title = "Почта"
+    content = {'title': title}
+    return render(request, 'mainapp/mail.html', content)
 
 def marketing(request):
-    return render(request, 'mainapp/marketing.html')
+    title = "Реклама"
+    content = {'title': title}
+    return render(request, 'mainapp/marketing.html', content)
 
 def realty(request):
-    return render(request, 'mainapp/realty.html')
+    title = "Недвижимость"
+    content = {'title': title}
+    return render(request, 'mainapp/realty.html', content)
 
 def staff_list(request):
-    return render(request, 'mainapp/staff_list.html')
+    title = "Сотрудники"
+    content = {'title': title}
+    return render(request, 'mainapp/staff_list.html', content)
 
 def staff_profile(request):
-    return render(request, 'mainapp/staff_profile.html')
+    title = "Личный профиль"
+    content = {'title': title}
+    return render(request, 'mainapp/staff_profile.html', content)
 
 def statistics(request):
-    return render(request, 'mainapp/statistics.html')
+    title = "Статистика"
+    content = {'title': title}
+    return render(request, 'mainapp/statistics.html', content)
 
 def team(request):
-    return render(request, 'mainapp/team.html')
+    title = "Команда"
+    content = {'title': title}
+    return render(request, 'mainapp/team.html', content)
